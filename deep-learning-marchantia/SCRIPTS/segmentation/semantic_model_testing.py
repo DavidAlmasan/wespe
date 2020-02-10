270cf746e4b5a3dc2969937bdec70d05db64d5b8
DavidAlmasan <davidalmasan@gmail.com>
2020-02-10 10:49:56 +0000

    converting script to module to be imported

--- deep-learning-marchantia/SCRIPTS/segmentation/semantic_model_testing.py
+++ deep-learning-marchantia/SCRIPTS/segmentation/semantic_model_testing.py
@@ -13,49 +13,49 @@ from model_arch import UNet16
 
 import torchvision.transforms.functional as TF
 
-##########################
+def test_semantic_model(inputImgPath, modelPath, saveImgPath):
+    num_feature_dim = 16
+    model = UNet16(num_feature_dim, pretrained=True)
 
-# This script allows you to test your semantic segmentation model with a test image.
+    model.load_state_dict(torch.load(modelPath))
+    model.eval()
 
-# What input image do you want to test?
-input_img_filepath = '../data/g1_t036_c001.png'
+    device = torch.device("cuda:0")
 
-# Where is the model you want to test?
-model_filepath = '../semseg_model_100epochs_16fdim_unet16_bce.pt'
+    model = model.to(device)
 
-# Where would you like to save the image?
-save_img_location = './sem_test_result.png'
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
 
-num_feature_dim = 16
+    with torch.no_grad():
+        image = Image.open(inputImgPath)
+        print(image.shape)
+        image = image.convert("RGB")
+        print(image.shape)
+        image = np.asarray(image, dtype=np.float32) / 255
+        image = image[:, :, :3]
+        
+        image = transform(image)
+        image = image.unsqueeze(0)
 
-##########################
+        image = image.to(device)
+        output = model(image)
 
-model = UNet16(num_feature_dim, pretrained=True)
+        output_img = output[0]
+        torchvision.utils.save_image(output_img, saveImgPath)
+        
+if __name__ == "__main__":
+    ##########################
 
-model.load_state_dict(torch.load(model_filepath))
-model.eval()
+    # This script allows you to test your semantic segmentation model with a test image.
 
-device = torch.device("cuda:0")
+    # What input image do you want to test?
+    input_img_filepath = '../data/g1_t036_c001.png'
 
-model = model.to(device)
+    # Where is the model you want to test?
+    model_filepath = '../semseg_model_100epochs_16fdim_unet16_bce.pt'
 
-transform = transforms.Compose([
-    transforms.ToTensor()
-])
-
-with torch.no_grad():
-    image = Image.open(input_img_filepath)
-
-    image = image.convert("RGB")
-    image = np.asarray(image, dtype=np.float32) / 255
-    image = image[:, :, :3]
-    
-    image = transform(image)
-    image = image.unsqueeze(0)
-
-    image = image.to(device)
-    output = model(image)
-
-    output_img = output[0]
-    torchvision.utils.save_image(output_img, save_img_location)
-    
+    # Where would you like to save the image?
+    save_img_location = './sem_test_result.png'
+    test_semantic_model(input_img_filepath, model_filepath, save_img_location)
