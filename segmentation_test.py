27807120a665e26c3da5f85628aff5f673c828f4
M.T. Dayao <mtd38@cam.ac.uk>
2020-02-10 15:04:54 +0000

    semseg enh results

--- segmentation_test.py
+++ segmentation_test.py
@@ -2,23 +2,23 @@ import subprocess
 import deep_learning_marcanthia.SCRIPTS.segmentation.semantic_model_testing as sem_model
 import os
 
-subprocess.call(['python' 'model.py'])
+subprocess.call(['python', 'model.py'])
 
 # Segment original image
 seg_folder = './deep_learning_marcanthia/SCRIPTS/segmentation'
 rel_to_main_folder = '../../../'
 relModelPath = '../semseg_model_100epochs_16fdim_unet16_bce.pt'
-saveImgPath = './model_tests/images/' + 'original_segmented.png'
+saveImgPath = './model_tests/' + 'original_segmented.png'
+relTestImgPath = './test_data/g2_t002_c001.png'
 inputPath = os.path.join(rel_to_main_folder, relTestImgPath)
-print(inputPath)
 saveImgPath = os.path.join(rel_to_main_folder, saveImgPath)
 sem_model.test_semantic_model(inputImgPath=inputPath,
                                 modelPath = relModelPath,
                                 saveImgPath = saveImgPath)
 
 # Segment enhanced image
-saveImgPath = './model_tests/images/' + timeName + 'enhanced_segmented.png'
-inputPath = os.path.join(rel_to_main_folder, './model_tests/images/' +  'enhanced_image.png')
+saveImgPath = './model_tests/' +  'enhanced_segmented.png'
+inputPath = os.path.join(rel_to_main_folder, './model_tests/' +  'enhanced_image.png')
 saveImgPath = os.path.join(rel_to_main_folder, saveImgPath)
 sem_model.test_semantic_model(inputImgPath=inputPath,
                                 modelPath = relModelPath,
