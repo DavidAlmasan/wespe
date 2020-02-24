d4339691a480bc385b5bebd64efaa54bcffc2995
M.T. Dayao <mtd38@cam.ac.uk>
2020-02-24 19:54:41 +0000

    wespe enhanced seg

--- segmentation_test.py
+++ segmentation_test.py
@@ -9,7 +9,7 @@ seg_folder = './deep_learning_marcanthia/SCRIPTS/segmentation'
 rel_to_main_folder = '../../../'
 relModelPath = '../semseg_model_100epochs_16fdim_unet16_bce.pt'
 saveImgPath = './model_tests/' + 'original_segmented.png'
-relTestImgPath = './test_data/img.png'
+relTestImgPath = './test_data/img.jpg'
 inputPath = os.path.join(rel_to_main_folder, relTestImgPath)
 saveImgPath = os.path.join(rel_to_main_folder, saveImgPath)
 sem_model.test_semantic_model(inputImgPath=inputPath,
@@ -17,9 +17,9 @@ sem_model.test_semantic_model(inputImgPath=inputPath,
                                 saveImgPath = saveImgPath)
 
 # Segment enhanced image
-#saveImgPath = './model_tests/' +  'enhanced_segmented.png'
-#inputPath = os.path.join(rel_to_main_folder, './model_tests/' +  'enhanced_image.png')
-#saveImgPath = os.path.join(rel_to_main_folder, saveImgPath)
-#sem_model.test_semantic_model(inputImgPath=inputPath,
-#                                modelPath = relModelPath,
-#                                saveImgPath = saveImgPath)
+saveImgPath = './model_tests/' +  'enhanced_segmented.png'
+inputPath = os.path.join(rel_to_main_folder, './model_tests/' +  'enhanced_image.png')
+saveImgPath = os.path.join(rel_to_main_folder, saveImgPath)
+sem_model.test_semantic_model(inputImgPath=inputPath,
+                              modelPath = relModelPath,
+                              saveImgPath = saveImgPath)
